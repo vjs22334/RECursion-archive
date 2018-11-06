07be3cd01e6939659b7bf055f77ddf4181e2795e
vjs2234 <jeyasurya.v@gmail.com>
2018-11-06 15:00:30 +0530

    added question and tag models

---
+++ archive/archive_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Question,Tag
+# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    readonly_fields = ['addedOn','lastModified','addedBy']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
---
+++ archive/archive_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Question(models.Model):
+    difficulties = [
+        ('B',"Beginner"),
+        ("E","Easy"),
+        ("M","Medium"),
+        ("H","Hard"),
+        ("O","other")
+    ]
+    questionName = models.CharField(max_length=100)
+    difficulty = models.CharField(choices = difficulties, max_length=50)
+    difficulty_description = models.CharField(max_length=100)
+    questionLink = models.URLField(max_length=500)
+    solutionLink = models.URLField(max_length=500)
+    summary = models.TextField()
+    addedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    addedOn = models.DateTimeField(auto_now=False, auto_now_add=True)
+    lastModified = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.questionName
+        
+
+class Tag(models.Model):
+    tagname = models.CharField(max_length=50)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.tagname
