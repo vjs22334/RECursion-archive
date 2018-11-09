e912dd199fc7c1aaf9c4fbc4013b2017947abc0a
ash1969 <nikhil.sonksbs@gmail.com>
2018-11-09 21:28:45 +0530

    Basic CRUD added!

--- archive/archive_app/urls.py
+++ archive/archive_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import list_questions, create_question, update_question, delete_question
+
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', list_questions ,name='list_questions'),
+    path('new', create_question , name='create_questions'),
+    path('update/<int:id>/', update_question, name='update_question'),
+    path('delete/<int:id>/', delete_question, name='delete_question'),
 ]
--- archive/archive_app/views.py
+++ archive/archive_app/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.template import loader, RequestContext
+from .forms import QuestionForm
 
 # Create your views here.
-def index(request):
-    return HttpResponse("this is the index page")
+def list_questions(request):
+    questions = Question.objects.all()
+    return render(request, 'questions.html', {
+        'questions': questions
+    })
+
+def create_question(request):
+    form = QuestionForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_questions')
+
+    return render(request, 'questions-form.html', {'form': form})
+
+def update_question(request, id):
+    question = Question.objects.get(id=id)
+    form = QuestionForm(request.POST or None, instance=question)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_questions')
+
+    return render(request, 'questions-form.html', {'form': form, 'question': question})
+
+def delete_question(request, id):
+    question=Question.objects.get(id=id)
+
+    if request.method == 'POST':
+        question.delete()
+        return redirect('list_questions')
+
+    return render(request, 'ques-delete-confirm.html', {'question': question})
